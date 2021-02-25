33f6a1ea5c25551acd127d9a10191a76ef659621
izzanrasyid <>
2021-02-26 01:52:23 +0700

    merubah tampilan

--- index.js
+++ index.js
@@ -50,7 +50,12 @@ let tweets = [
   }
 
   function editTweet(event){
+    const pBaru = document.createElement('p')
+    pBaru.classList.add("tweet-item");
+    const tekbaru = document.createTextNode('paragraf baru')
     
+    console.log(tekbaru)
+    pBaru.appendChild(tekbaru)
   }
   
   function render() {
@@ -59,13 +64,27 @@ let tweets = [
       const tweet = document.createElement("div");
       tweet.classList.add("tweet");
 
+      const profil = document.createElement("div")
+      profil.classList.add("profile")
+      tweet.appendChild(profil)
+
+      const pp = document.createElement("img");
+      pp.classList.add("ava-twitter")
+      pp.setAttribute("src", "https://static.appvn.com/a/uploads/thumbnails/112015/mr-square_icon.png");
+      profil.appendChild(pp)
+        
+      const username = document.createElement("p");
+      username.innerText = "Izzan Rasyid @izzanrasyid";
+      username.classList.add("username");
+      profil.appendChild(username);
+      
       const newtweet = document.createElement("li");
       newtweet.innerText = `${tweetListObj[i].task}`;
       newtweet.classList.add("tweet-item");
       tweet.appendChild(newtweet);
-
+      
       const deleteButton = document.createElement("button");
-      deleteButton.innerHTML = "Delete Tweet";
+      deleteButton.innerHTML = "Delete";
       deleteButton.classList.add("delete-btn");
       deleteButton.setAttribute("type", "submit")
 
